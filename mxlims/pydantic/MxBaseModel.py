22120287f2f36843e4552a234d6dd2006ff1b4a7
rhfogh <rhfogh@zoho.eu>
2025-04-30 10:53:53 +0100

    Added reverseLinkName, mxlimsBaseType, and spec for Id attributes

--- mxlims/pydantic/MxBaseModel.py
+++ mxlims/pydantic/MxBaseModel.py
@@ -1,4 +1,5 @@
-from typing import ClassVar
+import abc
+from typing import Any, ClassVar
 from weakref import WeakValueDictionary
 from pydantic import BaseModel as PydanticBaseModel
 
@@ -10,8 +11,31 @@ class BaseModel(PydanticBaseModel):
         use_enum_values = True
         validation_error_cause = True
 
-    # Containers for implementation access-object-by-id
-    datasets_by_id: ClassVar[WeakValueDictionary] = WeakValueDictionary()
-    jobs_by_id: ClassVar[WeakValueDictionary] = WeakValueDictionary()
-    prepared_samples_by_id: ClassVar[WeakValueDictionary] = WeakValueDictionary()
-    logistical_samples_by_id: ClassVar[WeakValueDictionary] = WeakValueDictionary()
+    # Class-level container for implementation of access-object-by-id
+    _objects_by_id: ClassVar[dict] = {
+        "Dataset": WeakValueDictionary(),
+        "Job": WeakValueDictionary(),
+        "PreparedSample": WeakValueDictionary(),
+        "LogisticalSample": WeakValueDictionary(),
+    }
+
+    @property
+    @abc.abstractmethod
+    def uuid(self):
+        pass
+
+    @property
+    @abc.abstractmethod
+    def mxlims_base_type(self):
+        pass
+
+    def __init__(self, /, **data: Any) -> None:
+        super().__init__(**data)
+        obj_by_id = self._objects_by_id[self.mxlims_base_type]
+        uid = self.uuid
+        if uid in obj_by_id:
+            raise ValueError(
+                f"{self.mxlims_base_type} with uuid '{uid}' already exists"
+            )
+        else:
+            obj_by_id[uid] = self
